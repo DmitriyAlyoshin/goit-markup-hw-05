add2b7ce66c332c30c868f2cefeb3a0f6fbf7c9b
Dmitriy Alyoshin <valerich1715@gmail.com>
2022-12-04 14:05:06 +0100

    Disable scroll

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,23 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+
+(() => {
+  const refs = {
+    buttonOff: document.querySelector("[data-modal-open]"),
+    buttonOn: document.querySelector("[data-modal-close]"),
+    body: document.body,
+  }
+
+  refs.buttonOff.addEventListener('click', disableScroll);
+
+  refs.buttonOn.addEventListener('click', enableScroll);
+
+  function disableScroll() {
+    refs.body.classList.add("disable-scroll");
+  }
+
+  function enableScroll() {
+    refs.body.classList.remove("disable-scroll");
+  }
+})();
